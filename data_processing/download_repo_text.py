bf11c8a8e622e932deec4bfe8693ab96e237a416
Daniel Fried <dfried@berkeley.edu>
2021-11-13 19:46:06 -0800

    add support for gitlab (and bitbucket, untested)

--- data_processing/download_repo_text.py
+++ data_processing/download_repo_text.py
@@ -10,83 +10,58 @@ import traceback
 import shutil
 import csv
 import json
+from collections import Counter
 from multiprocessing import cpu_count, Pool
 from tqdm import tqdm
 import argparse
 import subprocess
 from itertools import repeat
+import functools
 
-bad_extensions = [
-    'app',
-    'bin',
-    'bmp',
-    'bz2',
-    'class',
-    'csv',
-    'dat',
-    'db',
-    'dll',
-    'dylib',
-    'egg',
-    'eot',
-    'exe',
-    'gif',
-    'gitignore',
-    'glif',
-    'gradle',
-    'gz',
-    'ico',
-    'jar',
-    'jpeg',
-    'jpg',
-    'lo',
-    'lock',
-    'log',
-    'mp3',
-    'mp4',
-    'nar',
-    'o',
-    'ogg',
-    'otf',
-    'p',
-    'pdf',
-    'png',
-    'pickle',
-    'pkl',
-    'pyc',
-    'pyd',
-    'pyo',
-    'rkt',
-    'so',
-    'ss',
-    'svg',
-    'tar',
-    'tsv',
-    'ttf',
-    'war',
-    'webm',
-    'woff',
-    'woff2',
-    'xz',
-    'zip',
-    'zst'
-]
+from hacky_linguist import COMMON_LANGUAGES, EXTENSION_TO_LANGUAGE
+
+OPEN_SOURCE_LICENSES = {
+    # gitlab
+    'mit', 'apache-2.0', 'bsd-3-clause', 'bsd-3-clause-clear', 'bsd-2-clause', 
+    # github
+    'MIT License',
+    'Apache License 2.0',
+    'BSD 3-Clause New or Revised License',
+    'BSD 2-Clause Simplified License',
+    'BSD 3-Clause Clear License',
+    # licensee
+    'BSD 3-Clause "New" or "Revised" License',
+    'BSD 2-Clause "Simplified" License',
+}
+
+LICENSE_STANDARDIZATION = {
+    # github
+    'MIT License': 'mit',
+    'Apache License 2.0': 'apache-2.0',
+    'BSD 3-Clause New or Revised License': 'bsd-3-clause',
+    'BSD 2-Clause Simplified License': 'bsd-2-clause',
+    'BSD 3-Clause Clear License': 'bsd-3-clause-clear',
+    # licensee
+    'BSD 3-Clause "New" or "Revised" License': 'bsd-3-clause',
+    'BSD 2-Clause "Simplified" License': 'bsd-2-clause',
+}
+
+def build_language_extensions(filename='./Programming_Languages_Extensions_filtered.json'):
+    lang_exts = []
+    with open(filename) as f:
+        for i in json.load(f):
+            if "extensions" not in i:
+                continue
+            lang_exts.extend(i["extensions"])
+    return lang_exts
+
 # load programming language extensions from json file
-with open("./Programming_Languages_Extensions_filtered.json", "r") as f:
-    data = json.load(f)
+LANGUAGE_EXTENSIONS = build_language_extensions()
 
-lang_exts = []
-for i in data:
-    if "extensions" not in i:
-        continue
-    lang_exts.extend(i["extensions"])
-
-mime = magic.Magic(mime=True)
-
+MIME = magic.Magic(mime=True)
 
 class TimeoutError(Exception):
     pass
-
 
 def timeout(func, args=(), kwargs={}, timeout_duration=150, default=None):
     # wrap any function in this wrapper to raise a TimeoutError after timeout_duration secs
@@ -107,32 +82,13 @@ def timeout(func, args=(), kwargs={}, timeout_duration=150, default=None):
 
     return result
 
-
 def split_into_chunks(l, n):
     n = max(1, n)
     return [l[i:i + n] for i in range(0, len(l), n)]
 
 
-def is_digit(x):
-    return x in "1234567890"
-
-
-def keep(x):
-    # simple filters to decide whether a file is worth keeping
-    num_digits = len(list(filter(is_digit, x)))
-    num_newlines = len(list(filter(lambda x: x == '\n', x)))
-    if num_digits / len(x) > 0.8:
-        return False
-
-    # avg line length
-    if len(x) / (num_newlines + .001) > 200:
-        return False
-
-    return True
-
-
 def filter_by_stars(repo_data, n_stars):
-    return [item for item in repo_data if int(item[1]) >= n_stars]
+    return [record for record in repo_data if int(repo['stargazers']) >= n_stars]
 
 
 def get_content(f):
@@ -140,17 +96,12 @@ def get_content(f):
 
     type = None
     try:
-        enc = 'utf-8'
-        type = mime.from_file(f)
+        type = MIME.from_file(f)
         if not type.startswith('text'):
             return
         with open(f, 'rb') as fromfh:
             buf = fromfh.read()
-
         buf = buf.decode('UTF-8')
-        if not keep(buf):
-            return
-
         return buf
     except UnicodeDecodeError:
         # bad encoding, try different encoding
@@ -160,8 +111,6 @@ def get_content(f):
             if enc['encoding'] is None:
                 return
             buf = buf.decode(enc['encoding'])
-            if not keep(buf):
-                return
             return buf
         except UnicodeDecodeError:
             return
@@ -172,7 +121,7 @@ def get_content(f):
         import os.path
         if not os.path.islink(f):
             # something went horribly wrong!
-            ...
+            pass
 
 # def filter_criteria(files):
 #     filtered_files = []
@@ -183,12 +132,38 @@ def get_content(f):
 #             '.min.' not in f and f.split('.')[-1] not in bad_extensions and \
 #             f.split('.')[-1] in lang_exts and size
 
-def _process_repo(repo_data, repodir):
+def detect_licenses(repodir):
+    result = subprocess.run(f"licensee detect --json {repodir}", shell=True, stdout=subprocess.STDOUT, stderr=subprocess.STDERR)
+    try:
+        license_data = json.loads(result)
+        return [d['meta']['title'] for d in license_data['licenses']]
+    except Exception as e:
+        print(e, file=sys.stderr)
+        return []
+
+def remove_prefix(string, prefix):
+    assert string.startswith(prefix)
+    return string[len(prefix):]
+
+def _process_repo(repo_data, repodir, license_filter):
     out = None
     # get metadata
-    name, stars, lang, licen = repo_data
-    meta = {'repo_name': name, 'stars': stars, 'repo_language': lang, 'license': licen}
+    meta = repo_data.copy()
+    # for backward compatibility
+    meta['repo_name'] = meta['name']
+
+    #meta = {'repo_name': name, 'stars': stars, 'repo_language': lang, 'license': licen}
     try:
+        if 'license' not in meta:
+            licenses = [LICENSE_STANDARDIZATION.get(license, license) for license in detect_licenses(repodir)]
+            meta['detected_licenses'] = licenses
+        else:
+            meta['license'] = LICENSE_STANDARDIZATION.get(meta['license'], meta['license'])
+            licenses = [meta['license']]
+        if license_filter is not None:
+            # ensure this repo has a license and all licenses are within the filter
+            if not licenses or any(license not in license_filter for license in licenses):
+                return None
         for curdir, dirs, files in os.walk(repodir):
             # size = os.path.getsize('C:\\Python27\\Lib\\genericpath.py')
             filenames = []
@@ -200,13 +175,13 @@ def _process_repo(repo_data, repodir):
                 if len(split_ext) < 2:
                     continue
                 extension = split_ext[-1]
-                if extension not in lang_exts or extension in bad_extensions:
+                if extension not in LANGUAGE_EXTENSIONS:
                     continue
                 if '.git' in short_file_path or short_file_path[0] == '.' or 'LICENSE' in short_file_path or 'node_modules' in short_file_path or '.min.' in short_file_path:
                     continue
 
                 try:
-                    mime_type = mime.from_file(full_file_path)
+                    mime_type = MIME.from_file(full_file_path)
                 except FileNotFoundError:
                     mime_type = "n/a"
 
@@ -214,7 +189,8 @@ def _process_repo(repo_data, repodir):
                     text = get_content(full_file_path)
                 except TimeoutError:
                     raise TimeoutError
-                except:
+                except Exception as e:
+                    print(e)
                     text = None
 
                 if text is not None and text.strip():
@@ -231,20 +207,26 @@ def _process_repo(repo_data, repodir):
     return out
 
 
-def process_repo(repo_data, repodir, processing_timeout):
-    return timeout(_process_repo, args=(repo_data, repodir), timeout_duration=processing_timeout)
-
-
-def process_repo_list(repo_data, clone_timeout, processing_timeout):
+def process_repo_list(repo_data, clone_timeout, processing_timeout, source='github', license_filter=None):
     out = None
     try:
-        name, stars, lang, license = repo_data
-        username, projectname = name.split("/")
+        name = repo_data['name']
+        lang = repo_data['main_language']
+        if source == 'github':
+            base_url = f'https://github.com/{name}'
+        elif source == 'gitlab':
+            base_url = f'https://gitlab.com/{name}.git'
+        elif source == 'bitbucket':
+            base_url = f'https://bitbucket.org/{name}.git'
+        else:
+            raise ValueError(f"invalid source {source}")
+        # gitlab allows this to have more than 2
+        username, projectname = name.split("/")[-2:]
         rootfolder = os.path.join(".tmp", username)
         os.makedirs(rootfolder, exist_ok=True)
         repodir = os.path.join(rootfolder, projectname)
         # clones master branch of repos with depth 1 (most recent commit only), ignoring any terminal prompts
-        git_command = f'GIT_TERMINAL_PROMPT=0 git clone --depth 1 --single-branch https://github.com/{name} {projectname}'
+        git_command = f'GIT_TERMINAL_PROMPT=0 git clone --depth 1 --single-branch {base_url} {projectname}'
         p = subprocess.Popen(
             git_command,
             shell=True,
@@ -258,21 +240,19 @@ def process_repo_list(repo_data, clone_timeout, processing_timeout):
             p.kill()
         shutil.rmtree(f'{repodir}/.git', ignore_errors=True)
         # extracts text files from repo and returns them as list : [[text, metadata], ... ]
-        out = process_repo(repo_data, repodir, processing_timeout=processing_timeout)
+        out = timeout(_process_repo, args=(repo_data, repodir, license_filter), timeout_duration=processing_timeout)
     except Exception as e:
         print(e)
         # if verbose:
         #     print(e)
-    return out
+    return out, repo_data
 
 
 def process_args():
     parser = argparse.ArgumentParser(
-        description='CLI for github downloader - A tool for scraping repos as text from github')
-    parser.add_argument('input_csv')
+        description='CLI for git SCM downloader - A tool for scraping repos as text from github/gitlab/bitbucket')
     parser.add_argument('output_dir')
-    parser.add_argument('--already_scraped_input', help='path to file containing names of already scraped repos (to read from)')
-    parser.add_argument('--already_scraped_output', help='path to file containing names of already scraped repos (to write to)')
+    parser.add_argument('--input_csvs', nargs="+", required=True)
     parser.add_argument('--n_threads', help='number of threads for parallel processing, -1 for cpu_count * 3',
                         default=10,
                         type=int)
@@ -291,7 +271,9 @@ def process_args():
     parser.add_argument('--commit_freq', help='how often (in number of chunks) to commit the archive file',
                         default=10,
                         type=int)
+    parser.add_argument('--source', choices=['github', 'gitlab', 'bitbucket'], default='github')
     parser.add_argument('-v', '--verbose', help='if flag is present, print errors', action='store_true')
+    parser.add_argument('--open_source_only', action='store_true')
     return parser.parse_args()
 
 
@@ -300,28 +282,59 @@ if __name__ == '__main__':
     args = process_args()  # parse args
     verbose = args.verbose
 
-    # read repo data to a tuple (reponame, n_stars, language, license)
-    with open(args.input_csv, 'r') as f:
-        csv_reader = csv.reader(f)
-        row = next(csv_reader)
-        assert tuple(row) == ('name', 'stargazers', 'main_language', 'license')
-        repo_data = list(map(tuple, csv_reader))
-
-        # possibly filter out repos from the file in --already_scraped_input
-        if args.already_scraped_input is not None:
-            with open(args.already_scraped_input, 'r') as f:
-                already_scraped = set(l.strip() for l in f.readlines())
-            repo_data_filtered = [t for t in repo_data if t[0] not in already_scraped]
-            print(f"skipping {len(repo_data) - len(repo_data_filtered)} repos from {args.already_scraped_input}")
-            repo_data = repo_data_filtered
-
     if not os.path.isdir(args.output_dir):
         raise Exception("output directory {args.output_dir} does not exist; exiting")
 
-    if args.already_scraped_output is not None:
-        already_scraped_output_file = open(args.already_scraped_output, 'a', buffering=1)
+    already_processed_file = os.path.join(args.output_dir, "repos_processed.txt")
+    if os.path.exists(already_processed_file):
+        with open(already_processed_file, 'r') as f:
+            already_processed = set(l.strip() for l in f.readlines())
     else:
-        already_scraped_output_file = None
+        already_processed = {}
+
+    if args.open_source_only:
+        license_filter = OPEN_SOURCE_LICENSES
+    else:
+        license_filter = None
+
+    repo_data = []
+
+    to_process = set()
+
+    for input_csv in args.input_csvs:
+        with open(input_csv, 'r') as f:
+            csv_reader = csv.DictReader(f)
+            this_repo_data = list(csv_reader)
+
+        # possibly filter out repos from the file in --already_scraped_input
+        repo_data_filtered = []
+
+        skipped = Counter()
+
+        for t in this_repo_data:
+            if args.source == 'gitlab':
+                t['name'] = remove_prefix(t['url'], 'https://gitlab.com/')
+
+            assert 'name' in t, f"csv keys {t.keys()} do not include 'name'"
+            assert 'main_language' in t, f"csv keys {t.keys()} do not include 'main_language'"
+
+            if t['name'] in already_processed: 
+                skipped['processed'] += 1
+                continue
+            if t['name'] in to_process:
+                skipped['duplicate'] += 1
+                continue
+            if 'license' in t and license_filter is not None and t['license'] not in license_filter:
+                skipped['license'] += 1
+                continue
+            to_process.add(t['name'])
+            repo_data_filtered.append(t)
+        print(f"{input_csv}:\tskipping {sum(skipped.values())} repos \t {skipped.most_common()}")
+        repo_data.extend(repo_data_filtered)
+
+    print(f"{len(repo_data)} repos to process")
+
+    already_scraped_output_file = open(already_processed_file, 'a', buffering=1)
 
     print(args.output_dir)
     os.chdir(args.output_dir)
@@ -329,13 +342,13 @@ if __name__ == '__main__':
     # make output dirs
     if '.tmp' not in os.listdir():
         os.makedirs('.tmp')
-    if 'github_data' not in os.listdir():
-        os.makedirs('github_data')
+    if 'data' not in os.listdir():
+        os.makedirs('data')
 
     # filter by number of stars
     if args.n_stars != -1:
         repo_data = filter_by_stars(repo_data, args.n_stars)
-    repo_data.sort()
+    repo_data.sort(key=lambda t: t['name'])
 
     random.seed(420)
     random.shuffle(repo_data)
@@ -347,36 +360,47 @@ if __name__ == '__main__':
 
     # do work
     repo_chunks = split_into_chunks(repo_data, chunk_size)
-    archive_name = 'github_data'
+    archive_name = 'data'
     ar = lmd.Archive(archive_name)
+    processed_names = []
+
     pool = Pool(n_threads)
     pbar = tqdm(repo_chunks, total=len(repo_chunks), ncols=80)
     success_hist = []
+
+    processing_function = functools.partial(
+        process_repo_list, clone_timeout=args.clone_timeout, processing_timeout=args.processing_timeout,
+        source=args.source, license_filter=license_filter,
+    )
+
     for count, chunk in enumerate(pbar):
-        repos_out = pool.starmap(process_repo_list,
-                                 zip(chunk, repeat(args.clone_timeout), repeat(args.processing_timeout)))
         not_none = 0
         none = 0
-        for repo in repos_out:
-            if repo is not None:
+        repos_out = pool.map(processing_function, chunk)
+        for processed_files, repo_data in repos_out: 
+            if processed_files is not None:
                 not_none += 1
-                for f in repo:
+                for f in processed_files:
                     try:
                         ar.add_data(f[0], meta=f[1])
                     except UnicodeEncodeError as e:
                         continue
-                if already_scraped_output_file is not None:
-                    already_scraped_output_file.write(f[1]["repo_name"]+"\n")
             else:
                 none += 1
+            processed_names.append(repo_data['name'])
 
         # remove any leftover files
         subprocess.Popen("rm -rfv .tmp && mkdir .tmp", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
         if count % args.commit_freq == 0:
             ar.commit()
+            for name in processed_names:
+                already_scraped_output_file.write(name+"\n")
+            already_scraped_output_file.flush()
+            processed_names = []
         success_hist.append((not_none / len(repos_out)) * 100)
         success_rate = sum(success_hist) / len(success_hist)
         pbar.set_postfix({"Success Rate": success_rate})
     ar.commit() # final commit
-    if already_scraped_output_file is not None:
-        already_scraped_output_file.close()
+    for name in processed_names:
+        already_scraped_output_file.write(name+"\n")
+    already_scraped_output_file.close()
