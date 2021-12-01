9b6436462f30b913c8e9d3e5cabef6728823cf96
Daniel Fried <dfried@berkeley.edu>
2021-12-01 11:41:56 -0800

    add separate jupyter preprocessing script

--- code_clippy_dataset/jupyter_notebook_processing.py
+++ code_clippy_dataset/jupyter_notebook_processing.py
@@ -1,9 +1,10 @@
 from collections import namedtuple
+import functools
 from markdown import Markdown
 from io import StringIO
 import random
 
-from code_clippy_dataset.utils import make_tagged
+from utils import make_tagged
 
 # https://stackoverflow.com/a/54923798/1319683
 def unmark_element(element, stream=None):
@@ -91,3 +92,87 @@ def notebook_to_text(notebook_dictionary, options: JupyterOptions):
             for data_type, data in item.filtered_data.items():
                 strings.append(make_tagged('output', '\n'.join(data), {'type': data_type}, attribute_drop_probability=options.attribute_drop_probability))
     return '\n'.join(strings)
+
+
+def process_file(filename, output_dir, jupyter_options):
+    import zstandard as zstd
+    import io
+    import jsonlines
+    import json
+    import os
+    dctx = zstd.ZstdDecompressor()
+
+    basename = os.path.basename(filename)
+
+    cctx = zstd.ZstdCompressor(level=3, threads=4)
+
+    total_records = 0
+    valid_records = 0
+
+    out_path = os.path.join(output_dir, basename)
+    assert out_path != filename, "out_path: {out_path}"
+    
+    with open(filename, "rb") as f_in, open(out_path, 'wb') as f_out:
+        compressor = cctx.stream_writer(f_out)
+        def write_record(record, flush=False):
+            compressor.write(json.dumps(record).encode('UTF-8') + b'\n')
+            if flush:
+                compressor.flush(zstd.FLUSH_FRAME)
+                f_out.flush()
+
+        f = dctx.stream_reader(f_in)
+        f = io.TextIOWrapper(f, encoding="utf-8")
+        f = jsonlines.Reader(f)
+        for record_ix, record in enumerate(f):
+            meta = record["meta"]
+            fname = meta["file_name"]
+            print(fname)
+            _, extension = os.path.splitext(fname)
+            text = record["text"]
+            valid = True
+            if extension == '.ipynb':
+                try:
+                    notebook_dictionary = json.loads(text)
+                    text = notebook_to_text(notebook_dictionary, jupyter_options)
+                except Exception as e:
+                    valid = False
+                record['notebook_processed'] = True
+                record["text"] = text
+            if valid:
+                write_record(record, flush=(record_ix+1)%1000==0)
+                valid_records += 1
+            total_records += 1
+    print(f"{filename}: {valid_records} / {total_records} = {valid_records/total_records*100:.2f}%")
+    return (filename, valid_records, total_records)
+
+if __name__ == "__main__":
+    import argparse
+    import glob
+    from multiprocessing import Pool
+    import tqdm
+    import os
+    import sys
+    import pprint
+
+    print(' '.join(sys.argv))
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_dir")
+    parser.add_argument("output_dir")
+    parser.add_argument("--num_procs", type=int, default=20)
+
+    args = parser.parse_args()
+    pprint.pprint(vars(args))
+
+    input_files = glob.glob(os.path.join(args.input_dir, "*.jsonl.zst"))
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    fn = functools.partial(process_file, output_dir=args.output_dir, jupyter_options=DEFAULT_JUPYTER_OPTIONS_NO_OUTPUTS)
+
+    with Pool(args.num_procs) as p:
+        processed = tqdm.tqdm(p.imap(fn, input_files), total=len(input_files), ncols=80)
+
+    print("**complete**")
+
+    for filename, valid_records, total_records in processed:
+        print(f"{filename}: {valid_records} / {total_records} = {valid_records/total_records*100:.2f}%")
