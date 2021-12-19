7aa1ef2192ef16e3fb7dec5a755e83be8b4da1df
Daniel Fried <dfried@berkeley.edu>
2021-12-18 17:22:15 -0800

    add to bpe script

---
+++ to_bpe_single.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --time=4320
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --partition=devlab
+#SBATCH --mem-per-cpu=10G
+
+input_dir=$1
+
+python code_bpe_encoder.py \
+    --merge-file /checkpoint/dpf/data/tokenizers/github-py+so_psno-True/merges.txt \
+    --vocab-file /checkpoint/dpf/data/tokenizers/github-py+so_psno-True/vocab.json \
+    --pretokenizer-split-newlines-only \
+    --input-dirs ${input_dir} \
+    --output-dir /checkpoint/dpf/data/processed \
+    --use-hf-tokenizer \
+    --splits-dir /checkpoint/dpf/data/splits/ \
+    --workers 40
+
